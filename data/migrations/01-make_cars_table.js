8cb842c6c9b9de1d94129f289d3fdfcf59c3b717
Matthew Arofin <Matt.arofin@gmail.com>
2022-07-19 16:19:38 -0400

    Initial Schema setup

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,17 @@
 exports.up = function (knex) {
   // DO YOUR MAGIC
+  return knex.schema.createTable('Cars', tbl => {
+    tbl.increments();
+    tbl.varchar('vin', 40).notNullable();
+    tbl.varchar('make', 40).notNullable();
+    tbl.varchar('model', 40).notNullable();
+    tbl.integer('mileage').notNullable();
+    tbl.varchar('title');
+    tbl.varchar('transmission');
+  })
 };
 
 exports.down = function (knex) {
   // DO YOUR MAGIC
+  return knex.schema.dropTableIfExists('Cars');
 };
